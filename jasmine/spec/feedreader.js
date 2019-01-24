9f71f0d234996e9b21f0747f48f898f04837978f
Pushpamala Bangaru <b.pushpamala@gmail.com>
2019-01-23 22:23:46 -0800

    Added new test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -27,7 +27,7 @@
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
@@ -39,7 +39,7 @@
         });
 
 
-        /* TODO: Write a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
@@ -52,9 +52,9 @@
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* A new test suite named "The menu" */
 
-    /* TODO: Write a test that ensures the menu element is
+    /* A test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
@@ -65,7 +65,7 @@
             expect(bodyEl.classList).toContain('menu-hidden');
         });
 
-         /* TODO: Write a test that ensures the menu changes
+         /* A test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
@@ -81,12 +81,12 @@
          });
     });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+    /* A new test suite named "Initial Entries" */
     describe('Initial Entries', function() {            
 
         let oldFeedHtml = '';
         let nextFeedIndex = 1;
-        /* TODO: Write a test that ensures when the loadFeed
+        /* A test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -106,10 +106,21 @@
             let entryEl = document.querySelector('.feed .entry');
             expect(entryEl).toBeDefined();
          });
+    });     
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
-
-        /* TODO: Write a test that ensures when a new feed is loaded
+    /* A new test suite named "New Feed Selection" */
+    describe('New Feed Selection', function() {
+        let oldFeedHtml = '';
+        let newFeedIndex = 2;
+        beforeEach(function(done) {
+            let feedEl = document.querySelector('.feed');
+            oldFeedHtml = feedEl.innerHTML;
+            feedEl.innerHTML = '';
+            loadFeed(newFeedIndex, function() {
+                done();
+            });
+        });
+        /* A test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
